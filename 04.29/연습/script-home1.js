762381719dbdc0da38fd6928ac6b71f530bc8661
hyunji0129 <https://github.com/hyunji0129/web2-por>
2026-04-29 21:16:02 +0900

    4.29

---
+++ 04.29/연습/script-home1.js
@@ -0,0 +1,57 @@
+// function removeBanner(){
+//     const banner = document.querySelector("#top-banner");
+
+//     if(banner){
+//         banner.remove();
+//         console.log("베너가 제거 되었습니다");
+//     }
+// }
+
+// const deleteButtons = document.querySelectorAll(".btn-delete");
+// const cartList = document.querySelector("#cart-list");
+// const empTyMsg = document.querySelector("#empty-cart");
+
+// deleteButtons.forEach((button) => {
+
+//     button.addEventListener("click", function(event){
+
+//         const item = event.target.closest(".cart-item");
+
+//         if(item){
+//             item.remove();
+//         }
+
+//         if(cartList.children.length === 0){
+//             empTyMsg.computedStyleMap.display = "block";
+//         }
+//     });
+// });
+
+function removeBanner(){
+    const banner = document.querySelector("#top-banner");
+
+    if(banner){
+        banner.remove();
+        console.log("베너가 삭제되었습니다.");
+    }
+}
+
+const deleteButtons = document.querySelectorAll(".btn-delete");
+const cartList = document.querySelector("#cart-list");
+const empTyMsg = document.querySelector("#empty-cart");
+
+deleteButtons.forEach((button) => {
+
+    button.addEventListener("click", function(event){
+
+        const item = event.target.closest(".cart-item");
+
+        if(item){
+            item.remove();
+        }
+
+        if(cartList.children.length === 0){
+            empTyMsg.computedStyleMap.display = "block";
+        }
+    });
+});
---
+++ 04.29/연습/script-home2.js
@@ -0,0 +1,29 @@
+// const tabs = document.querySelectorAll(".tab-button");
+// const resultText = document.querySelector("#selected-category");
+
+// tabs.forEach((tab) => {
+//     tab.addEventListener("click", function(event){
+//         const clickedTarget = event.target;
+
+//         tabs.forEach((btn) => btn.classList.remove("active"));
+//         clickedTarget.classList.add("active");
+//         resultText.textContent = clickedTarget.innerText;
+
+//         console.log("클릭된 요소:", clickedTarget);
+//     });
+// });
+
+const tabs = document.querySelectorAll(".tab-button");
+const resultText = document.querySelector("#selected-category");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", function(event){
+        const clickedTarget = event.target;
+
+        tabs.forEach((btn) => btn.classList.remove("active"));
+        clickedTarget.classList.add("active");
+        resultText.textContent = clickedTarget.innerText;
+
+        console.log("클릭된 요소:", clickedTarget);
+    });
+});
